bcece422cedc4cc0932503b52d8338110ca3467e
Zsolt Deák <lordmairtis@gmail.com>
2021-10-06 21:47:19 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'hubsson-parking';
+export class AppComponent implements OnInit {
+  dates: { date: Date; today?: boolean }[] | undefined;
+  today: Date | undefined;
+
+  @HostBinding('class.app-root') hostCss = true;
+
+  private readonly msPerDay = 1000 * 60 * 60 * 24;
+  private readonly days = Array(7).fill(0);
+
+  ngOnInit() {
+    this.today = new Date();
+    const todayIndex = this.today.getDay() - 1;
+    const todayTime = this.today.getTime();
+    this.dates = this.days.map((_, dayIndex: number) => {
+      const dateDiff = dayIndex - todayIndex;
+      const timeForDayIndex = todayTime + dateDiff * this.msPerDay;
+      return { date: new Date(timeForDayIndex) };
+    });
+    this.dates[todayIndex].today = true;
+  }
 }
