705f90c2eeb5a9489832c8dc597ac3e91e111b36
Zsolt Deák <lordmairtis@gmail.com>
2021-10-09 16:15:13 +0200

    kinda fine UI firebase workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,52 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-  dates: { date: Date; today?: boolean }[] | undefined;
+  days: { date: Date }[] | undefined;
   today: Date | undefined;
+  readonly parkingSlots = [
+    { name: 'Hubsson1', id: '0' },
+    { name: 'Hubsson2', id: '1' },
+    { name: 'Páva1', id: '1' },
+    { name: 'Páva2', id: '1' },
+    { name: 'Páva3', id: '1' },
+    { name: 'Páva4', id: '1' },
+    { name: 'Páva5', id: '1' },
+    { name: 'Páva6', id: '1' },
+    { name: 'Páva7', id: '1' },
+    { name: 'Páva8', id: '1' },
+    { name: 'Páva9', id: '1' },
+    { name: 'Páva10', id: '1' },
+  ];
 
   @HostBinding('class.app-root') hostCss = true;
 
   private readonly msPerDay = 1000 * 60 * 60 * 24;
-  private readonly days = Array(7).fill(0);
+  private newReservations: Record<number, number> = {};
 
   ngOnInit() {
     this.today = new Date();
-    const todayIndex = this.today.getDay() - 1;
     const todayTime = this.today.getTime();
-    this.dates = this.days.map((_, dayIndex: number) => {
-      const dateDiff = dayIndex - todayIndex;
-      const timeForDayIndex = todayTime + dateDiff * this.msPerDay;
-      return { date: new Date(timeForDayIndex) };
-    });
-    this.dates[todayIndex].today = true;
+    this.days = Array(10)
+      .fill(0)
+      .map((_, dayIndex: number) => {
+        const timeForDayIndex = todayTime + dayIndex * this.msPerDay;
+        return { date: new Date(timeForDayIndex) };
+      });
+  }
+
+  changeTakeParkingSlot(event: Event, column: number, row: number) {
+    if (!(event.target as HTMLInputElement)?.checked) {
+      return;
+    }
+    this.newReservations[column] = row;
+  }
+
+  onSave() {
+    // TODO
+  }
+
+  isSlotFree(date: Date, parkingSlotId: string): boolean {
+    // TODO
+    return date && !!parkingSlotId;
   }
 }
