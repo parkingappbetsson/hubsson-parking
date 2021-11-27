ce8b2fb84ce56715e8f6fb038f994e4c933e5c52
Zsolt Deák <lordmairtis@gmail.com>
2021-11-27 21:07:33 +0100

    material secret code and user-chooser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,24 @@ import { AppComponent } from './app.component';
 import { ParkingComponent } from './parking/parking.component';
 import { SecretCodeComponent } from './secret-code/secret-code.component';
 import { UserChooserComponent } from './user-chooser/user-chooser.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
 	declarations: [AppComponent, ParkingComponent, SecretCodeComponent, UserChooserComponent],
-	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		NoopAnimationsModule,
+		MatButtonModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatSelectModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
