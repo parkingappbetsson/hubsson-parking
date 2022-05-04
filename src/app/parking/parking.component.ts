4be4931a435f2913cf232438cfd6c20ee5fd7b01
Zsolt Deák <lordmairtis@gmail.com>
2022-05-04 09:29:34 +0200

    on popular request changing to 8 days

--- src/app/parking/parking.component.ts
+++ src/app/parking/parking.component.ts
@@ -26,7 +26,7 @@ type UserId = string;
 type ParkingSlotId = string;
 type ReservationDayDate = number;
 type UserIdByParkingSlotId = Record<ParkingSlotId, UserId>;
-const NUMBER_OF_DAYS = 7;
+const NUMBER_OF_DAYS = 8;
 
 @Component({
 	selector: 'hp-parking',
