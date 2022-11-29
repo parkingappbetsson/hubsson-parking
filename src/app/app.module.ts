1c9042aee19a12e8334510d01907de334ab99d47
László Kiss <laki01@betssongroup.com>
2022-11-29 16:54:07 +0100

    feat(*): add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { BadgeComponent } from './badge/badge.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [AppComponent, ParkingComponent, SecretCodeComponent, UserChooserComponent, BadgeComponent],
@@ -35,6 +37,12 @@ import { BadgeComponent } from './badge/badge.component';
 		MatProgressSpinnerModule,
 		HttpClientModule,
 		MatIconModule,
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: environment.production,
+    // Register the ServiceWorker as soon as the app is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
